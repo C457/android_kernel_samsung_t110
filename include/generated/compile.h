955bcb1cec2099593ad966b371de2f240e3ff5a5
C457 <android.c357@gmail.com>
2014-02-01 16:56:36 -0600

    Clean kernel don't be messy samsung

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Mon Jan 13 15:01:05 KST 2014"
-#define LINUX_COMPILE_BY "omsgsm"
-#define LINUX_COMPILE_HOST "sy918-desktop"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
